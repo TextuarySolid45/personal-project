f91d9b79d8a5f851e381dfc89a0d1878bd0b4110
TextuarySolid45 <11gabriel11lopez@gmail.com>
2022-12-07 12:29:06 -0800

    wip

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Gabriel David Lopez';
+export const SITE_DESCRIPTION = 'Personal Website to showcase for business and family';
